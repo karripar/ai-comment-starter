748dfab9ddfb10c9198f2410c5585b1e740fa251
karripar <karripartanen25@gmail.com>
2025-09-12 12:00:41 +0300

    added stuff

--- src/api/controllers/commentController.ts
+++ src/api/controllers/commentController.ts
@@ -1,17 +1,52 @@
 import {Request, Response, NextFunction} from 'express';
+import CustomError from '../../classes/CustomError';
+import fetchData from '../../lib/fetchData';
+import { ChatCompletion } from 'openai/resources/index';
 
 const commentPost = async (
   req: Request<{}, {}, {text: string}>,
   res: Response<{response: string}>,
   next: NextFunction
 ) => {
-  try {
     try {
       // TODO: Generate a response to a Youtube comment
-      // Instead of using openai library, use fetchData to make a post request to the server.
-      // see https://platform.openai.com/docs/api-reference/chat/create for more information
-      // You don't need an API key if you put the URL provided in Oma to .env.sample and Metropolia VPN
-      // Example: instad of https://api.openai.com/v1/chat/completions use process.env.OPENAI_API_URL + '/v1/chat/completions'
+      const request = {
+        messages: [
+          {
+            role: 'system',
+            content: 'Olet keppihevosien harrastaja, joka vastaa kysymyksiin keppihevosista. Vastaa lyhyesti, ytimekkäästi ja töykeästi käyttäen kiroilua aina kun mahdollista.',
+          },
+          {
+            role: 'user',
+            content: req.body.text,
+          }
+        ],
+        model: 'gpt-5-nano',
+      }
+
+      if (!process.env.OPENAI_API_URL) {
+        next(new CustomError('Missing OPENAI_API_URL in .env', 500));
+        return;
+      }
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request),
+      };
+
+      const response = await fetchData<ChatCompletion>(process.env.OPENAI_API_URL + '/v1/chat/completions', options);
+
+      console.log(response.choices[0].message.content);
+
+      if (!response.choices || response.choices.length === 0) {
+        next(new CustomError('No response from AI', 500));
+        return;
+      }
+
+      res.json({response: response.choices[0].message?.content || 'No response'});
     } catch (error) {
     next(error);
   }
